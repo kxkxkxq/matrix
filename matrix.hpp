36ae15d3a5d9748bbb89249828bc5a9ab5d8734e
?kxkxk_q <sukhanov.is@phystech.edu>
2025-02-20 01:58:04 +0300

    some changes in matrix.hpp

--- matrix.hpp
+++ matrix.hpp
@@ -1,52 +1,55 @@
 #pragma once
+#include <cstddef>
 
 namespace matrices
 {
-    template <typename T> class Matrix
+    template <typename T> class SquareMatrix
     {
-        class Row_
+        template <typename U> class Buffer_
         {
-            T* pRow_;
-            const size_t rowLength_; 
+            U* pBuf_;
+            const size_t bufSize_; 
             
+            void swap(Buffer_& rhs) noexcept;
+
         public :
 
-            Row_(const size_t rl) : pRow_(new T[rl]), rowLength_(rl) {};
+            Buffer_(const size_t size = 0) : pBuf_(new U[size]), bufSize_(size) {};
+            Buffer_(const size_t size, U val);
+            Buffer_(const size_t size, U& val);
             
-            Row_(const Row_ &rhs);
-            Row_(const Row_ &&rhs);
+            Buffer_(const Buffer_& rhs);
+            Buffer_(Buffer_&& rhs);
 
-            Row_& operator=(const Row_& rhs);
-            Row_& operator=(Row_& rhs);
+            Buffer_& operator=(const Buffer_& rhs);
+            Buffer_& operator=(Buffer_&& rhs);
 
-            ~Row_() {delete[] pRow_;};
+            ~Buffer_() {delete[] pBuf_;};
 
-            const size_t size() const noexcept {return rowLength_;};
+            const size_t size() const noexcept {return bufSize_;};
             
-            T& front() {return *pRow_;};
-            const T& front() {return *pRow_;};
+            U& front() {return pBuf_[0];};
+            const U& front() const {return pBuf_[0];};
 
-            T& back() {return *(pRow_ + rowLength_ - 1);};
-            const T& back() {return *(pRow_ + rowLength_ - 1);};
+            U& back() {return pBuf_[bufSize_ - 1];};
+            const U& back() const {return pBuf_[bufSize_ - 1];};
         
-            T& operator[](const int indx) {return pRow_[indx];};
-            const T& operator[](const int indx) const {return pRow_[indx];};
+            U& operator[](const int indx) {return pBuf_[indx];};
+            const U& operator[](const int indx) const {return pBuf_[indx];};
 
         };
 
-        Row_* matrixRows_;
-        const size_t numRows_;
-        const size_t numColumns_; 
+        Buffer_<Buffer<T>> contSqMatrix_;
+        const size_t matrixSize_; 
 
     public :
 
-        const size_t num_of_rows() const noexcept {return numRows_;};
-        const size_t num_of_columns() const noexcept {return numColumns_;};
+        const size_t size() const noexcept {return matrixSize_;};
 
         template <typename Iter> 
-        Matrix( const size_t numRows, const size_t numColumns, const Iter begin, const Iter end);
+        SquareMatrix( const size_t size, const Iter begin, const Iter end);
         
-        Matrix(const size_t numRows, const size_t numColumns, T val = T{});
+        SquareMatrix(const size_t size, T val = T{});
 
         const long determinant();
 
@@ -77,8 +80,53 @@ namespace matrices
 
 
 
+//  MATRIX::BUFFER_ 
+
+    template <typename T> 
+    template <typename U> 
+    SquareMatrix<T>::Buffer_<U>::Buffer_(const SquareMatrix<T>::Buffer_<U>& rhs)
+    {
+        pBuf_ = new(U[rhs.bufSize_]);
+        try
+        {
+            for(size_t i = 0; i < rhs.bufSize_; ++i)
+                pBuf_[i] = rhs.pBuf_[i];
+        }
+        catch(...)
+        {
+            delete [] pBuf_;
+            throw;
+        }
+
+        bufSize_ = rhs.bufSize_; 
+    }
+
+    template <typename T> 
+    template <typename U> 
+    SquareMatrix<T>::Buffer_<U>::Buffer_(SquareMatrix<T>::Buffer_<U>&& rhs)
+    {
 
 
+
+    }
+
+    template <typename T> 
+    template <typename U> 
+    void 
+    SquareMatrix<T>::Buffer_<U>::swap(SquareMatrix<T>::Buffer_<U>& rhs) noexcept
+    {
+        std::swap(pBuf_, rhs.pBuf_);
+        std::swap(bufSize_, rhs.bufSize_);
+    }
+
+    template <typename T> 
+    template <typename U> 
+    SquareMatrix<T>::Buffer_<U>& 
+    SquareMatrix<T>::Buffer_<U>::operator=(const SquareMatrix<T>::Buffer_<U>& rhs)
+    {
+        Buffer<U> tmp{rhs};
+        swap(tmp);
+    }
 
 
 
