282853f36f66587c56c5f5adee0b45d7e36dba8c
?kxkxk_q <sukhanov.is@phystech.edu>
2025-03-11 01:19:47 +0300

    [tests] add generator the generator has not been used yet, so it contains many bugs

--- tests/end-to-end-tests/generator.cpp
+++ tests/end-to-end-tests/generator.cpp
@@ -1,3 +1,39 @@
 //  this programme generates matrices of a given size and
 //  determinant value, which will later be used in e2e tests
+//  and outputs the size of the obtained matrix and its elements
+//  line by line to stdout 
 
+#include <iostream>
+
+#include "generator.hpp"
+
+int main()
+{
+    long long matrixSize = 0;
+    std::cin >> matrixSize;
+    if(std::cin.fail() || (matrixSize < 0) )
+    {
+        std::cout << "Incorrect input of matrix size" << std::endl;
+        if(matrixSize < 0)
+            std::cerr << "The matrix size must not be less than 0" << std::endl;
+        return 1;
+    } 
+
+    double determinant = 0;
+    std::cin >> determinant;
+    if(std::cin.fail())
+    {
+        std::cout << "Incorrect input of matrix determinant" << std::endl;
+        return 1;
+    }  
+
+    MatrixGenerator<int> mg{static_cast<size_t>(matrixSize), determinant};
+    mg.generate_matrix();
+    std::vector<int> resultMatrixVec = mg.get_matrix_vec();
+
+    std::cout << matrixSize << std::endl;
+    for(auto&& i : resultMatrixVec)
+        std::cout << *i << " ";
+    std::cout << std::endl;
+    return 0;
+}
